1496ab2920d3d55c22d2f08d01176bcd238f4d76
Ryomanba <datemanba3@gmail.com>
2022-03-31 16:36:19 +0900

    #27 refactor: vectorの機能テスト

--- test/test_vector.cpp
+++ test/test_vector.cpp
@@ -134,145 +134,117 @@ bool test_const_reverse_iterator(void)
         ft_ans.push_back(*ft_rit);
     }
     if (vector_comp(std_ans, ft_ans)) return false;
+
     return true;
 }
 
 // Capacity
 bool test_size(void)
 {
-    std::vector<int> vec1;
-    ft::vector<int> myvec1;
+    std::vector<int> std_vec1;
+    ft::vector<int> ft_vec1;
+    if (std_vec1.size() != ft_vec1.size()) return false;
 
-    if (vec1.size() != myvec1.size())
-    {
-        return false;
-    }
-    vec1.push_back(1);
-    myvec1.push_back(1);
-    if (vec1.size() != myvec1.size())
-    {
-        return false;
-    }
+    std::vector<int> std_vec2(3);
+    ft::vector<int> ft_vec2(3);
+    if (std_vec2.size() != ft_vec2.size()) return false;
 
-    std::vector<int> vec2(5);
-    ft::vector<int> myvec2(5);
-    if (vec2.size() != myvec2.size())
-    {
-        return false;
-    }
+    std::vector<int> std_vec3(5, 5);
+    ft::vector<int> ft_vec3(5, 5);
+    if (std_vec3.size() != ft_vec3.size()) return false;
+
     return true;
 }
 
 bool test_max_size(void)
 {
-    std::vector<char> vec_char;
-    std::vector<signed char> vec_schar;
-    std::vector<wchar_t> vec_wchar;
-    std::vector<short> vec_short;
-    std::vector<unsigned short> vec_ushort;
-    std::vector<int> vec_int;
-    std::vector<unsigned int> vec_uint;
-    std::vector<long> vec_long;
-    std::vector<unsigned long> vec_ulong;
-    std::vector<std::string> vec_str;
+    std::vector<char> std_vec_char;
+    std::vector<signed char> std_vec_schar;
+    std::vector<wchar_t> std_vec_wchar;
+    std::vector<short> std_vec_short;
+    std::vector<unsigned short> std_vec_ushort;
+    std::vector<int> std_vec_int;
+    std::vector<unsigned int> std_vec_uint;
+    std::vector<long> std_vec_long;
+    std::vector<unsigned long> std_vec_ulong;
+    std::vector<std::string> std_vec_str;
 
-    ft::vector<char> myvec_char;
-    ft::vector<signed char> myvec_schar;
-    ft::vector<wchar_t> myvec_wchar;
-    ft::vector<short> myvec_short;
-    ft::vector<unsigned short> myvec_ushort;
-    ft::vector<int> myvec_int;
-    ft::vector<unsigned int> myvec_uint;
-    ft::vector<long> myvec_long;
-    ft::vector<unsigned long> myvec_ulong;
-    ft::vector<std::string> myvec_str;
+    ft::vector<char> ft_vec_char;
+    ft::vector<signed char> ft_vec_schar;
+    ft::vector<wchar_t> ft_vec_wchar;
+    ft::vector<short> ft_vec_short;
+    ft::vector<unsigned short> ft_vec_ushort;
+    ft::vector<int> ft_vec_int;
+    ft::vector<unsigned int> ft_vec_uint;
+    ft::vector<long> ft_vec_long;
+    ft::vector<unsigned long> ft_vec_ulong;
+    ft::vector<std::string> ft_vec_str;
 
-    if (vec_char.max_size() != myvec_char.max_size()) return false;
-    if (vec_schar.max_size() != myvec_schar.max_size()) return false;
-    if (vec_wchar.max_size() != myvec_wchar.max_size()) return false;
-    if (vec_short.max_size() != myvec_short.max_size()) return false;
-    if (vec_ushort.max_size() != myvec_ushort.max_size()) return false;
-    if (vec_int.max_size() != myvec_int.max_size()) return false;
-    if (vec_uint.max_size() != myvec_uint.max_size()) return false;
-    if (vec_long.max_size() != myvec_long.max_size()) return false;
-    if (vec_ulong.max_size() != myvec_ulong.max_size()) return false;
-    if (vec_str.max_size() != myvec_str.max_size()) return false;
+    if (std_vec_char.max_size() != ft_vec_char.max_size()) return false;
+    if (std_vec_schar.max_size() != ft_vec_schar.max_size()) return false;
+    if (std_vec_wchar.max_size() != ft_vec_wchar.max_size()) return false;
+    if (std_vec_short.max_size() != ft_vec_short.max_size()) return false;
+    if (std_vec_ushort.max_size() != ft_vec_ushort.max_size()) return false;
+    if (std_vec_int.max_size() != ft_vec_int.max_size()) return false;
+    if (std_vec_uint.max_size() != ft_vec_uint.max_size()) return false;
+    if (std_vec_long.max_size() != ft_vec_long.max_size()) return false;
+    if (std_vec_ulong.max_size() != ft_vec_ulong.max_size()) return false;
+    if (std_vec_str.max_size() != ft_vec_str.max_size()) return false;
 
     return true;
 }
 
 bool test_resize(void)
 {
-    std::vector<int> vec1;
-    ft::vector<int> myvec1;
+    std::vector<int> std_vec;
+    ft::vector<int> ft_vec;
 
-    vec1.resize(0);
-    myvec1.resize(0);
-    if (vec1.size() != myvec1.size())
-    {
-        return false;
-    }
+    std_vec.resize(0);
+    ft_vec.resize(0);
+    if (vector_comp(std_vec, ft_vec)) return false;
 
-    vec1.resize(10);
-    myvec1.resize(10);
-    if (vec1.size() != myvec1.size())
-    {
-        return false;
-    }
+    std_vec.resize(10);
+    ft_vec.resize(10);
+    if (vector_comp(std_vec, ft_vec)) return false;
 
-    std::vector<int> vec2(5);
-    std::vector<int> myvec2(5);
-    if (vec2.size() != myvec2.size())
-    {
-        return false;
-    }
+    std_vec.resize(15, 42);
+    ft_vec.resize(15, 42);
+    if (vector_comp(std_vec, ft_vec)) return false;
+
+    std_vec.resize(1, 42);
+    ft_vec.resize(1, 42);
+    if (vector_comp(std_vec, ft_vec)) return false;
+
     return true;
 }
 
 bool test_capacity(void)
 {
-    std::vector<int> vec1;
-    ft::vector<int> myvec1;
+    std::vector<int> std_vec1;
+    ft::vector<int> ft_vec1;
+    if (std_vec1.capacity() != ft_vec1.capacity()) return false;
 
-    vec1.resize(0);
-    myvec1.resize(0);
-    if (vec1.capacity() != myvec1.capacity()) return false;
-    vec1.resize(10);
-    myvec1.resize(10);
-    if (vec1.capacity() != myvec1.capacity()) return false;
-
-    std::vector<int> vec2(5);
-    ft::vector<int> myvec2(5);
-    if (vec2.capacity() != myvec2.capacity()) return false;
+    std::vector<int> std_vec2(5);
+    ft::vector<int> ft_vec2(5);
+    if (std_vec2.capacity() != ft_vec2.capacity()) return false;
 
     return true;
 }
 
 bool test_empty(void)
 {
-    std::vector<int> vec1;
-    ft::vector<int> myvec1;
+    std::vector<int> std_vec1;
+    ft::vector<int> ft_vec1;
+    if (std_vec1.empty() != ft_vec1.empty()) return false;
 
-    if (vec1.empty() != myvec1.empty())
-    {
-        return false;
-    }
+    std::vector<int> std_vec2(5);
+    ft::vector<int> ft_vec2(5);
+    if (std_vec2.empty() != ft_vec2.empty()) return false;
 
-    std::vector<int> vec2(5);
-    ft::vector<int> myvec2(5);
+    std::vector<int> std_vec3(0);
+    ft::vector<int> ft_vec3(0);
+    if (std_vec3.empty() != ft_vec3.empty()) return false;
 
-    if (vec2.empty() != myvec2.empty())
-    {
-        return false;
-    }
-
-    std::vector<int> vec3(0);
-    ft::vector<int> myvec3(0);
-
-    if (vec3.empty() != myvec3.empty())
-    {
-        return false;
-    }
     return true;
 }
 
@@ -325,10 +297,7 @@ bool test_at(void)
     }
     for (size_t i = 0; i < sz; ++i)
     {
-        if (std_vec.at(i) != ft_vec.at(i))
-        {
-            return false;
-        }
+        if (std_vec.at(i) != ft_vec.at(i)) return false;
     }
     return true;
 }
@@ -338,17 +307,13 @@ bool test_front(void)
     std::vector<int> std_vec(5);
     ft::vector<int> ft_vec(5);
 
-    if (std_vec.front() != ft_vec.front())
-    {
-        return false;
-    }
+    if (std_vec.front() != ft_vec.front()) return false;
     std_vec.front() = 5;
     ft_vec.front()  = 5;
 
-    if (std_vec.front() != ft_vec.front())
-    {
-        return false;
-    }
+    if (std_vec.front() != ft_vec.front()) return false;
+    if (vector_comp(std_vec, ft_vec)) return false;
+
     return true;
 }
 
@@ -357,16 +322,12 @@ bool test_back(void)
     std::vector<int> std_vec(5);
     ft::vector<int> ft_vec(5);
 
-    if (std_vec.back() != ft_vec.back())
-    {
-        return false;
-    }
+    if (std_vec.back() != ft_vec.back()) return false;
     std_vec.back() = 5;
     ft_vec.back()  = 5;
-    if (std_vec.back() != ft_vec.back())
-    {
-        return false;
-    }
+    if (std_vec.back() != ft_vec.back()) return false;
+    if (vector_comp(std_vec, ft_vec)) return false;
+
     return true;
 }
 
@@ -377,41 +338,44 @@ bool test_data(void)
 
     if (std_vec.data() != &std_vec[0]) return false;
     if (ft_vec.data() != &ft_vec[0]) return false;
+
     return true;
 }
 
 // Modilers
 bool test_assign(void)
 {
-    std::vector<int> vec1;
-    ft::vector<int> myvec1;
-    std::vector<int> vec2(5);
-    ft::vector<int> myvec2(5);
+    std::vector<int> std_vec1;
+    ft::vector<int> ft_vec1;
+    std::vector<int> std_vec2(5);
+    ft::vector<int> ft_vec2(5);
 
     // capacity以上
-    vec1.assign(5, 42);
-    myvec1.assign(5, 42);
+    std_vec1.assign(5, 42);
+    ft_vec1.assign(5, 42);
 
     // capacity以下
-    vec2.assign(vec2.size(), 42);
-    myvec2.assign(myvec2.size(), 42);
+    std_vec2.assign(std_vec2.size(), 42);
+    ft_vec2.assign(ft_vec2.size(), 42);
 
-    if (vector_comp(vec1, myvec1)) return false;
-    if (vector_comp(vec2, myvec2)) return false;
+    if (vector_comp(std_vec1, ft_vec1)) return false;
+    if (vector_comp(std_vec2, ft_vec2)) return false;
+
     return true;
 }
 
 bool test_push_back(void)
 {
-    const size_t sz = 5;
     std::vector<int> std_vec;
     ft::vector<int> ft_vec;
-    for (size_t i = 0; i < sz; ++i)
+
+    for (size_t i = 0; i < 5; ++i)
     {
         std_vec.push_back(i);
         ft_vec.push_back(i);
     }
     if (vector_comp(std_vec, ft_vec)) return false;
+
     return true;
 }
 
@@ -431,16 +395,8 @@ bool test_pop_back(void)
     {
         std_vec.pop_back();
         ft_vec.pop_back();
+        if (vector_comp(std_vec, ft_vec)) return false;
     }
-    if (vector_comp(std_vec, ft_vec)) return false;
-
-    sz = std_vec.size();
-    for (size_t i = 0; i < sz; ++i)
-    {
-        std_vec.push_back(i);
-        ft_vec.push_back(i);
-    }
-    if (vector_comp(std_vec, ft_vec)) return false;
 
     return true;
 }
@@ -448,40 +404,41 @@ bool test_pop_back(void)
 bool test_insert(void)
 {
     const size_t sz = 5;
-    std::vector<int> vec1(sz);
-    ft::vector<int> myvec1(sz);
+    std::vector<int> std_vec1(sz);
+    ft::vector<int> ft_vec1(sz);
 
     for (size_t i = 0; i < sz; ++i)
     {
-        vec1[i]   = i;
-        myvec1[i] = i;
+        std_vec1[i] = i;
+        ft_vec1[i]  = i;
     }
     // 要素2つ(it, pos)
-    std::vector<int>::iterator it  = vec1.insert(vec1.begin(), 1);
-    ft::vector<int>::iterator myit = myvec1.insert(myvec1.begin(), 1);
-    if (*it != *myit) return false;
-    if (vector_comp(vec1, myvec1)) return false;
+    std::vector<int>::iterator std_it = std_vec1.insert(std_vec1.begin(), 1);
+    ft::vector<int>::iterator ft_it   = ft_vec1.insert(ft_vec1.begin(), 1);
 
-    std::vector<int> vec2(sz);
-    ft::vector<int> myvec2(sz);
+    if (*std_it != *ft_it) return false;
+    if (vector_comp(std_vec1, ft_vec1)) return false;
+
+    std::vector<int> std_vec2(sz);
+    ft::vector<int> ft_vec2(sz);
 
     for (size_t i = 0; i < sz; ++i)
     {
-        vec2[i]   = i;
-        myvec2[i] = i;
+        std_vec2[i] = i;
+        ft_vec2[i]  = i;
     }
     // 要素3つ(it, digit, digit)
-    vec2.insert(vec2.begin() + 2, 2, 2);
-    myvec2.insert(myvec2.begin() + 2, 2, 2);
-    if (vector_comp(vec2, myvec2)) return false;
+    std_vec2.insert(std_vec2.begin() + 2, 2, 2);
+    ft_vec2.insert(ft_vec2.begin() + 2, 2, 2);
+    if (vector_comp(std_vec2, ft_vec2)) return false;
 
-    std::vector<int> vec3(sz);
-    ft::vector<int> myvec3(sz);
+    std::vector<int> std_vec3(sz);
+    ft::vector<int> ft_vec3(sz);
 
     // 要素3つ(it, other_it, other_it)
-    vec3.insert(vec3.begin(), vec2.begin(), vec2.end());
-    myvec3.insert(myvec3.begin(), vec2.begin(), vec2.end());
-    if (vector_comp(vec3, myvec3)) return false;
+    std_vec3.insert(std_vec3.begin(), std_vec2.begin(), std_vec2.end());
+    ft_vec3.insert(ft_vec3.begin(), std_vec2.begin(), std_vec2.end());
+    if (vector_comp(std_vec3, ft_vec3)) return false;
 
     return true;
 }
@@ -491,92 +448,76 @@ bool test_insert(void)
 bool test_erase(void)
 {
     const size_t sz = 5;
-    std::vector<int> vec1(sz);
-    ft::vector<int> myvec1(sz);
+    std::vector<int> std_vec1(sz);
+    ft::vector<int> ft_vec1(sz);
 
     for (size_t i = 0; i < sz; ++i)
     {
-        vec1[i]   = i;
-        myvec1[i] = i;
+        std_vec1[i] = i;
+        ft_vec1[i]  = i;
     }
-    std::vector<int>::iterator it;
-    ft::vector<int>::iterator myit;
+    std::vector<int>::iterator std_it;
+    ft::vector<int>::iterator ft_it;
 
     // 引数1つ
     for (size_t i = 0; i < sz; ++i)
     {
-        it   = vec1.erase(vec1.begin());
-        myit = myvec1.erase(myvec1.begin());
+        std_it = std_vec1.erase(std_vec1.begin());
+        ft_it  = ft_vec1.erase(ft_vec1.begin());
 
         if (i != sz - 1)
         {
-            if (*it != *myit) return false;
+            if (*std_it != *ft_it) return false;
         }
-        if (vector_comp(vec1, myvec1)) return false;
+        if (vector_comp(std_vec1, ft_vec1)) return false;
     }
 
     // 引数2つ
-    std::vector<int> vec2(sz);
-    ft::vector<int> myvec2(sz);
+    std::vector<int> std_vec2(sz);
+    ft::vector<int> ft_vec2(sz);
 
     for (size_t i = 0; i < sz; ++i)
     {
-        vec2[i]   = i;
-        myvec2[i] = i;
+        std_vec2[i] = i;
+        ft_vec2[i]  = i;
     }
-    it   = vec2.erase(vec2.begin(), vec2.begin() + 2);
-    myit = myvec2.erase(myvec2.begin(), myvec2.begin() + 2);
+    std_it = std_vec2.erase(std_vec2.begin(), std_vec2.begin() + 2);
+    ft_it  = ft_vec2.erase(ft_vec2.begin(), ft_vec2.begin() + 2);
 
-    if (*it != *myit) return false;
-    if (vector_comp(vec2, myvec2)) return false;
+    if (*std_it != *ft_it) return false;
+    if (vector_comp(std_vec2, ft_vec2)) return false;
 
-    it   = vec2.erase(vec2.begin(), vec2.end());
-    myit = myvec2.erase(myvec2.begin(), myvec2.end());
+    std_it = std_vec2.erase(std_vec2.begin(), std_vec2.end());
+    ft_it  = ft_vec2.erase(ft_vec2.begin(), ft_vec2.end());
 
-    if (vector_comp(vec2, myvec2)) return false;
+    if (vector_comp(std_vec2, ft_vec2)) return false;
 
     return true;
 }
 
 bool test_swap(void)
 {
-    std::vector<int> vec1(3);
-    std::vector<int> vec2(3);
-    ft::vector<int> myvec1(3);
-    ft::vector<int> myvec2(3);
+    std::vector<int> std_vec1(3);
+    std::vector<int> std_vec2(3);
+    ft::vector<int> ft_vec1(3);
+    ft::vector<int> ft_vec2(3);
 
     for (size_t i = 0; i < 3; ++i)
     {
-        vec1[i]   = i;
-        myvec1[i] = i;
+        std_vec1[i] = i;
+        ft_vec1[i]  = i;
     }
     for (size_t i = 0; i < 3; ++i)
     {
-        vec2[i]   = i + 3;
-        myvec2[i] = i + 3;
+        std_vec2[i] = i + 3;
+        ft_vec2[i]  = i + 3;
     }
+    std_vec1.swap(std_vec2);
+    ft_vec1.swap(ft_vec2);
 
-    vec1.swap(vec2);
-    myvec1.swap(myvec2);
+    if (vector_comp(std_vec1, ft_vec1)) return false;
+    if (vector_comp(std_vec2, ft_vec2)) return false;
 
-    if (vec1.size() != myvec1.size() || vec2.size() != myvec2.size())
-    {
-        return false;
-    }
-    for (size_t i = 0; i < vec1.size(); ++i)
-    {
-        if (vec1[i] != myvec1[i])
-        {
-            return false;
-        }
-    }
-    for (size_t i = 0; i < vec2.size(); ++i)
-    {
-        if (vec2[i] != myvec2[i])
-        {
-            return false;
-        }
-    }
     return true;
 }
 
@@ -587,10 +528,8 @@ bool test_clear(void)
 
     std_vec.clear();
     ft_vec.clear();
-    if (std_vec.empty() != ft_vec.empty())
-    {
-        return false;
-    }
+    if (std_vec.empty() != ft_vec.empty()) return false;
+
     return true;
 }
 
@@ -600,17 +539,15 @@ bool test_get_allocator(void)
     std::vector<int> std_vec(alloc);
     ft::vector<int> ft_vec(alloc);
 
-    if (std_vec.get_allocator() != ft_vec.get_allocator())
-    {
-        return false;
-    }
+    if (std_vec.get_allocator() != ft_vec.get_allocator()) return false;
+
     return true;
 }
 
-// TODO: 修正
 bool test_constructor(void)
 {
     const size_t sz = 5;
+
     // default constructor
     std::vector<int> std_vec1;
     ft::vector<int> ft_vec1;
@@ -647,7 +584,7 @@ bool test_constructor(void)
 bool test_operator_equal(void)
 {
     const size_t sz = 5;
-    // default constructor
+
     std::vector<int> std_vec1(sz);
     ft::vector<int> ft_vec1(sz);
     for (size_t i = 0; i < sz; ++i)
@@ -659,36 +596,46 @@ bool test_operator_equal(void)
     // 代入
     std::vector<int> std_vec2 = std_vec1;
     ft::vector<int> ft_vec2   = ft_vec1;
+    if (vector_comp(std_vec1, ft_vec1)) return false;
     if (vector_comp(std_vec2, ft_vec2)) return false;
+
     return true;
 }
 
 bool test_operator(void)
 {
+    std::vector<int> std_vec1;
+    std::vector<int> std_vec2;
+    std::vector<int> std_vec3;
+
     ft::vector<int> ft_vec1;
     ft::vector<int> ft_vec2;
     ft::vector<int> ft_vec3;
 
     for (size_t i = 0; i < 5; ++i)
     {
+        std_vec1.push_back(i);
+        std_vec2.push_back(i);
+        std_vec3.push_back(i + i);
+
         ft_vec1.push_back(i);
         ft_vec2.push_back(i);
         ft_vec3.push_back(i + i);
     }
 
-    if (!(ft_vec1 == ft_vec2)) return false;
-    if (ft_vec1 != ft_vec2) return false;
-    if (ft_vec1 < ft_vec2) return false;
-    if (!(ft_vec1 <= ft_vec2)) return false;
-    if (ft_vec2 > ft_vec1) return false;
-    if (!(ft_vec1 >= ft_vec2)) return false;
+    if ((std_vec1 == std_vec2) != (ft_vec1 == ft_vec2)) return false;
+    if ((std_vec1 != std_vec2) != (ft_vec1 != ft_vec2)) return false;
+    if ((std_vec1 < std_vec2) != (ft_vec1 < ft_vec2)) return false;
+    if ((std_vec1 <= std_vec2) != (ft_vec1 <= ft_vec2)) return false;
+    if ((std_vec1 > std_vec2) != (ft_vec2 > ft_vec1)) return false;
+    if ((std_vec1 >= std_vec2) != (ft_vec1 >= ft_vec2)) return false;
 
-    if (ft_vec1 == ft_vec3) return false;
-    if (!(ft_vec1 != ft_vec3)) return false;
-    if (!(ft_vec1 < ft_vec3)) return false;
-    if (!(ft_vec1 <= ft_vec3)) return false;
-    if (ft_vec1 > ft_vec3) return false;
-    if (ft_vec1 >= ft_vec3) return false;
+    if ((std_vec1 == std_vec3) != (ft_vec1 == ft_vec3)) return false;
+    if ((std_vec1 != std_vec3) != (ft_vec1 != ft_vec3)) return false;
+    if ((std_vec1 < std_vec3) != (ft_vec1 < ft_vec3)) return false;
+    if ((std_vec1 <= std_vec3) != (ft_vec1 <= ft_vec3)) return false;
+    if ((std_vec1 > std_vec3) != (ft_vec1 > ft_vec3)) return false;
+    if ((std_vec1 >= std_vec3) != (ft_vec1 >= ft_vec3)) return false;
 
     return true;
 }
@@ -696,7 +643,7 @@ bool test_operator(void)
 int test_vector(void)
 {
     Tester tester;
-    std::cout << "-----[TEST VECTOR]-----" << std::endl;
+    std::cout << "=======[TEST VECTOR]=======" << std::endl;
 
     // constructer
     tester.run(test_constructor(), "test_constructor");
