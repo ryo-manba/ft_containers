75d19bee15fb0db8b8b4619dc39dcaa8a0c9318c
Ryomanba <datemanba3@gmail.com>
2022-04-01 23:12:53 +0900

    #21 update: 実行速度が同じだった場合の例外処理

--- benchmark/show_diff.sh
+++ benchmark/show_diff.sh
@@ -3,12 +3,22 @@ awk '{
     GREEN="\033[32m"
     YELLOW="\033[33m"
     RESET="\033[m"
-    if( NR>1 )
+
+    if( $2 != "BENCHMARK" )
     {
-        diff = $2 / $6
-        if ( diff > 20 ) { printf "%s[KO] ", RED }
-        else { printf "%s[OK] ", GREEN }
-        printf "%s%6.2fx\033[m :\033[33m %s%s\n", \
-                RESET,($2 / $6), COLOR, $1
+        diff = 0
+        ans = "OK"
+        color = GREEN
+        if ( $4 == "|" )
+        {
+            diff = $2 / $6
+            if ( diff > 20 )
+            {
+                ans = "KO"
+                color = RED
+            }
+        }
+        printf "%s[%s] ",color, ans
+        printf "%s%6.2fx\033[m : %s %s\n", RESET, diff, YELLOW, $1
     }
 }' benchmark_diff.log
