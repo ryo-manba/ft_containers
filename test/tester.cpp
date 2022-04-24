d4ce479c13315c59756a4f624f0da09bf0c8c6a6
Ryomanba <datemanba3@gmail.com>
2022-04-24 15:42:27 +0900

    #78 fixes コンパイルエラー修正

--- test/tester.cpp
+++ test/tester.cpp
@@ -2,7 +2,10 @@
 
 #include <iostream>
 
-#include "color.hpp"
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define RESET "\033[0m"
 
 Tester::Tester()
     : ret_(0)
